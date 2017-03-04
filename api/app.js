f0ccdf22b85382b4acda078f4604495c1077caef
Communist <apvnsk@gmail.com>
2017-03-05 01:50:17 +0700

    Init babel and eslint for api

--- api/app.js
+++ api/app.js
@@ -1,27 +1,28 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+import express from 'express';
+import logger from 'morgan';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+
+    err.status = 404;
+
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-module.exports = app;
+export default app;
